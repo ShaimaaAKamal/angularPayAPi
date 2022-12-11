ab0fed53b62ccb07a2afa8d2c5da3e3797675a6e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-11 17:34:10 +0200

    navbar component logic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+   @ViewChild('close') close:any;
+   @ViewChild('menu') menu:any;
+   @ViewChild('collapse') navbarCollapse:any;
 
+   handleOpenMenu=()=>{
+    this.menu.nativeElement.classList.add('d-none');
+    this.close.nativeElement.classList.remove('d-none');
+    this.close.nativeElement.classList.add('adjustBtn');
+    this.navbarCollapse.nativeElement.classList.add('d-block');
+    this.navbarCollapse.nativeElement.classList.remove('d-none');
+  }
+   handleCloseMenu=()=>{
+    this.close.nativeElement.classList.add('d-none');
+    this.menu.nativeElement.classList.remove('d-none');
+    this.navbarCollapse.nativeElement.classList.add('d-none');
+    this.navbarCollapse.nativeElement.classList.remove('d-block');
+    this.close.nativeElement.classList.remove('adjustBtn');
+  }
 }
