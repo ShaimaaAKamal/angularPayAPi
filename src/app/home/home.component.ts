1035f7e12f2f028fc9cfedffeb731717fcd2c077
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 15:54:49 +0200

    define variables for home card section in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+ titles:string[]=['Personal Finances','Banking & Coverage','Consumer Payments'];
+ descriptions:string[]=[
+  `Consolidate financial data from multiple sources and categorize transactions up to 2 years of history. Analyze reports to reconcile activities in your account.`
+    ,`With our platform, you can speed up account onboarding and support ongoing payments for checking, savings, credit card, and brokerage accounts.`
+    ,`It’s easier to set up secure bank payments with us through a flow designed with the user experience in mind. Customers could instantly authenticate their account.`
+  ]
+  Images:string[]=[
+    '../../assets/Images/home/desktop/icon-personal-finances.svg',
+    '../../assets/Images/home/desktop/icon-banking-and-coverage.svg',
+    '../../assets/Images/home/desktop/icon-consumer-payments.svg'
 
+  ]
 }
