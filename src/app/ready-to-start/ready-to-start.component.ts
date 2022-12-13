3723b1161bf2317684d30e85495e834aba781531
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 16:27:41 +0200

    implement readu to start component

---
+++ src/app/ready-to-start/ready-to-start.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ready-to-start',
+  templateUrl: './ready-to-start.component.html',
+  styleUrls: ['./ready-to-start.component.scss']
+})
+export class ReadyToStartComponent {
+
+}
